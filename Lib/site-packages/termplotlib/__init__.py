f0e4a490178e26b85074f2902d2f0afdc73abaa7
smesharik <91406253+smeshar@users.noreply.github.com>
2024-11-27 14:04:12 +0300

    Delete Lib directory

--- Lib/site-packages/termplotlib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .figure import Figure, figure
-from .helpers import get_gnuplot_version
-from .subplot import SubplotGrid, subplot_grid
-
-__all__ = ["Figure", "figure", "SubplotGrid", "subplot_grid", "get_gnuplot_version"]
